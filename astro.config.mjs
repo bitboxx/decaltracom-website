110f62aa4c5a69c006776b037cf066b8fd1519e8
Martin Ted <martin@bitboxx.com>
2025-08-07 17:46:51 +0200

    First release version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 // @ts-check
+import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()],
-
+  site: 'https://pixeau.nl',
+  adapter: cloudflare({
+    imageService: 'passthrough'
+  }),
+  integrations: [mdx(), sitemap()],
+  output: 'server',
   vite: {
     // @ts-ignore - tailwindcss plugin type compatibility
     plugins: [tailwindcss()],
